d7e9e8acad4abb5de5985c7d9d588631b5697a8a
Deea Majed <deea1887@yahoo.com>
2021-04-09 19:45:08 +0300

    add tested code

--- server/tcp.js
+++ server/tcp.js
@@ -1,29 +1,40 @@
 var net = require('net');
 var server = net.createServer();    
 server.on('connection', handleConnection);
-server.listen(9000, function() {    
+server.listen(9000, function() 
+{    
     console.log('server listening to %j', server.address());  
-  });
+});
 
-  function handleConnection(conn) {    
-    var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
-    console.log('new client connection from %s', remoteAddress);
-    conn.on('data', onConnData);  
-    conn.once('close', onConnClose);  
-    conn.on('error', onConnError);
-    function onConnData(d) {  
-        console.log('connection data from %s: %j', remoteAddress, d);  
-        conn.write(d);  
-      }
-      function onConnClose() {  
-        console.log('connection from %s closed', remoteAddress);  
-      }
-      function onConnError(err) {  
-        console.log('Connection %s error: %s', remoteAddress, err.message);  
-      }  
-    }    
+A = null
+B = null
+function handleConnection(conn) 
+{    
+  if (!A)
+  {
+    A = conn
+  }
+  else if (!B)
+  {
+    B = conn
+  }
 
-    function onConnData(d) {  
-        console.log('connection data from %s: %j', remoteAddress, d);  
-        conn.write(d);  
-      }
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
+  console.log('new client connection from %s', remoteAddress);
+  sendData();
+}    
+function sendData ()
+{
+  if (A && B)
+  {
+    aMessage = {address  : B.remoteAddress , port  : B.remotePort};
+    bMessage = {address  : A.remoteAddress , port  : A.remotePort};
+    A.write(JSON.stringify(aMessage)+'\0');
+    B.write(JSON.stringify(bMessage)+'\0');
+  }
+}
+function onConnData(d) 
+{  
+  console.log('connection data from %s: %j', remoteAddress, d);  
+  conn.write(d);  
+}
